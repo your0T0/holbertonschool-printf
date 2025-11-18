e972965b168e491e5d6f5bb599073fce8958f21f
your0T0 <yara241020@gmail.com>
2025-11-18 20:04:52 +0000

    update printf project file

---
+++ functions.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _putchar -writes a character to stdout
+* @c: character to print
+* Return: 1 on success
+*/
+int _putchar(char c)
+{
+ return (write(1, &c, 1));
+}
+
+/**
+* print_string - prints a string
+* @str: string to print
+* Return: number of characters printed
+*/
+int print_string(char *str)
+{
+int count = 0;
+if (str == NULL)
+str = "(null)";
+while (str[count] != '\0')  
+_putchar(str[count]);
+count++;
+return (count);
+}
+
+/**
+* print _number - prints an integer
+* @n: Integer to print 
+* Return : Number of characters printed
+*/
