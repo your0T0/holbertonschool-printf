dee11a0c6feef4e11f4ee233ead9e0b11e1f4451
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-20 07:58:52 +0000

    the printf code was missing a few lines after second else

--- _printf.c
+++ _printf.c
@@ -8,55 +8,57 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int count = 0;
+	va_list args;
+	int i = 0, count = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] != '%')
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        else
-        {
-            i++;
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			i++;
 
-            if (format[i] == '\0')
-            {
-                va_end(args);
-                return (-1);
-            }
-            if (format[i] == 'c')
-            {
-                char c = (char)va_arg(args, int);
-                _putchar(c);
-                count++;
-            }
-            else if (format[i] == 's')
-            {
-                char *s = va_arg(args, char *);
-                count += print_string(s);
-            }
-            else if (format[i] == 'd' || format[i] == 'i')
-            {
-                int n = va_arg(args, int);
-                count += print_number(n);
-            }
-            else if (format[i] == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-            }
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			if (format[i] == 'c')
+			{
+				char c = (char)va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *);
+				count += print_string(str);
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
