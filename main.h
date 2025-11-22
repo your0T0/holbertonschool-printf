0001650194c4a0f16014ac8ae804760a98040931
your0T0 <yara241020@gmail.com>
2025-11-22 18:02:26 +0000

    Add prototypes for new number conversion functions (%u, %o, %x, %X)

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+int print_string(va_list args);
+int print_number(va_list args);
+int print_char(va_list args);
 int print_unsigned(va_list args);
 int print_octal(va_list args);
 int print_hex(va_list args, int uppercase)
